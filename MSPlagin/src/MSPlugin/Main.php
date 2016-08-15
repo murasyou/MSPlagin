1086f2eac6b8428a60655b2f1471750de0f96ea1
murasyou <humihko0128@yahoo.co.jp>
2016-08-15 21:51:11 +0900

    0.1.3

--- MSPlagin/src/MSPlugin/Main.php
+++ MSPlagin/src/MSPlugin/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MSPlagin;
+namespace MSPlugin;
 
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
@@ -10,46 +10,45 @@ use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 
-class MSPlagin extends PluginBase implements Listener
+class Main extends PluginBase implements Listener{
+
+ public function onEnable()
+    {
   if(!file_exists($this->getDataFolder())){//configを入れるフォルダが有るかチェック
     mkdir($this->getDataFolder(), 0744, true);//なければフォルダを作成
-}
-//引数1:configファイルまでの場所とconfigファイル名,引数2:configファイルのフォーマット
-//引数3:configファイルに入れるデータ
-$this->config = new Config($this->getDataFolder() . "SGMember.yml", Config::YAML,
+        $this->config = new Config($this->getDataFolder() . "SGMember.yml", Config::YAML,
+            array(
+                'test' => 'data',
+                'データ名(キー)' => '値'
+                ));
+        $this->config->save();
+        $this->configct = new Config($this->getDataFolder() . "CTMember.yml", Config::YAML,
 array(
         'test' => 'data',
         'データ名(キー)' => '値'
-));
- if(!file_exists($this->getDataFolder())){//configを入れるフォルダが有るかチェック
-    mkdir($this->getDataFolder(), 0744, true);//なければフォルダを作成
+        ));
+        $this->configct->save();
+    }
 }
-//引数1:configファイルまでの場所とconfigファイル名,引数2:configファイルのフォーマット
-//引数3:configファイルに入れるデータ
-$this->configct = new Config($this->getDataFolder() . "CTMember.yml", Config::YAML,
-array(
-        'test' => 'data',
-        'データ名(キー)' => '値'
-));
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-if ($sender instanceof Player) return $this->onCommandByUser($sender, $command, $label, $args);
+        if (!$sender instanceof Player) return false;
         switch ($command->getName()) {
            case "gs":
-                $subCommand = strtolower(array_shift($args));
+                $subCommand = strtolower(array_shift($args[0]));
                 switch ($subCommand) {
                     case "":
                     case "help":
                         $sender->sendMessage("/gs kick プレイヤー");
                         $sender->sendMessage("/gs sp ");
                         $sender->sendMessage("/gs c プレイヤー");
-　　　　　　　　　　　　$sender->sendMessage("/gs add プレイヤー");
+                        $sender->sendMessage("/gs add プレイヤー");
                         $sender->sendMessage("/gs del プレイヤー");
                         break;
 
                     case "add":
                         if($sender->isOp()){
-                        $target = array_shift($args);
-                        $this->config->set($player, "on");//値と名前を設定
+                        $target = array_shift($args[1]);
+                        $this->config->set($target, "on");//値と名前を設定
                         $this->config->save();//設定を保存
                         }else{
                         $sender->sendMessage("あなたに権限はありません");
@@ -58,8 +57,8 @@ if ($sender instanceof Player) return $this->onCommandByUser($sender, $command, 
 
                     case "del":
                         if($sender->isOp()){                       
-                        $target = array_shift($args);
-                        $this->config->set($player, "off");//値と名前を設定
+                        $target = array_shift($args[1]);
+                        $this->config->set($target, "off");//値と名前を設定
                         $this->config->save();//設定を保存
                         }else{
                         $sender->sendMessage("あなたに権限はありません");
@@ -68,12 +67,14 @@ if ($sender instanceof Player) return $this->onCommandByUser($sender, $command, 
 
                     case "kick":
                         $gsk = $this->config->get($sender);
-                        $target = array_shift($args);
-                        if ($gsk == on){
-                        $player->kick("警備員によるkick", true);//trueをfalseにすることで"警備員によるkick"と表示されなくなります
-                        
+                        $target = array_shift($args[1]);
+                        if ($gsk === "on"){
+                        $target->kick("警備員によるkick", true);//trueをfalseにすることで"警備員によるkick"と表示されなくなります  
                         }else{
                         $sender->sendMessage("あなたに権限はありません");
-　　　　　　　　　　　　}
+                    }
                         break;
-                        }}}
+                    }
+                }
+            }
+        }
